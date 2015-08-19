809a4670ee41f5b5fa43fb4a0232b7c3c04bb893
lany <liuyuyao1992@qq.com>
2015-08-19 19:59:15 +0800

    New ZigZagConversion

--- LeetCode/src/leetcode/Q6_ZigZagConversion.java
+++ LeetCode/src/leetcode/Q6_ZigZagConversion.java
@@ -1,7 +1,27 @@
 package leetcode;
 
 public class Q6_ZigZagConversion {
-	public String convert(String s, int numRows) {
-        
+	public static String convert(String s, int numRows) {
+        String ss= new String("");
+        int skip = 1;
+        int k = 0;
+        int now = 0;
+        for (int i=0; i<numRows; i++){
+        		if ((numRows-1) * 2!=0);
+        			skip = (numRows-1)*2;
+        		now = i;
+        		k = skip - i * 2;
+        		while (now < s.length()){
+        			if (k!=0)
+        				ss += s.charAt(now);
+        			now += k;
+        			k = skip -k;
+        		}
+        }
+        return ss;
     }
+	public static void main(String[] args) {
+		String s = "0";
+		System.out.println(convert(s,1));
+	}
 }
