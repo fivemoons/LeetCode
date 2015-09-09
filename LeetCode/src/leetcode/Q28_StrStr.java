acab339c7f8769b76ea0befa6f56701ce502a90e
fivemoons <liuyuyao1992@qq.com>
2015-09-09 17:54:48 +0800

    New Q28_StrStr with KMP

--- LeetCode/src/leetcode/Q28_StrStr.java
+++ LeetCode/src/leetcode/Q28_StrStr.java
@@ -4,8 +4,71 @@ public class Q28_StrStr {//找子串 最普通的方法。。。
 	public static int strStr(String haystack, String needle) {
         return haystack.indexOf(needle);
     }
+	public static int[] getNext(String p){
+		int[] next = new int[p.length()];
+		next[0] = -1; //初始值为-1
+		int k = -1; //k是当前的最长真后缀
+		int j = 0; //循环用
+		while (j < p.length() - 1){   //遍历第一个点到倒数第二个点
+			if ((k == -1)||(p.charAt(j) == p.charAt(k))){ //匹配到的情况
+				j++; //下标加1
+				k++; //真后缀加1
+				next[j] =  k; //当前j的next设为k
+			}
+			else{
+				k = next[k]; //没有匹配到，k退回到前一个真后缀，然后
+			}
+		}
+		return next;
+	}
+	
+	public static int[] getNext2(String p){
+		int[] next = new int[p.length()];
+		next[0] = -1;
+		int k = -1;
+		int j = 0;
+		while (j < p.length() - 1){
+			if ((k == -1)||(p.charAt(j) == p.charAt(k))){
+				j++;
+				k++;
+				if (p.charAt(j)!=p.charAt(k))
+					next[j] =  k;
+				else
+					next[j] =next[k];
+			}
+			else{
+				k = next[k];
+			}
+		}
+		return next;
+	}
+	
+	
+	public static int KMP(String haystack, String needle){
+		if ("".equals(needle)) return 0;
+		int[] next = getNext(needle);
+		for (int i=0; i< next.length; i++){
+			System.out.print(next[i] + "_");
+		}
+		System.out.println();
+		int k = 0;
+		int j = 0;
+		while ((k<haystack.length())&&(j < needle.length())){
+			if ((j == -1)||(haystack.charAt(k) == needle.charAt(j))){
+				k++;
+				j++;
+			}
+			else{
+				j = next[j];
+			}
+		}
+		if (j == needle.length())
+			return k - j;
+		else
+			return -1;
+	}
 	public static void main(String[] args) {
-		System.out.println(strStr("abcdefg","cde"));
+		System.out.println(KMP("",""));
 	}
 
 }
