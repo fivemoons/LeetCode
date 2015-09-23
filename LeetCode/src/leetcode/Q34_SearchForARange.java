03b8e97d652f52c461f6c55116ddf92787622d57
fivemoons <liuyuyao1992@qq.com>
2015-09-23 09:18:00 +0800

    New Q34_SearchForARange

---
+++ LeetCode/src/leetcode/Q34_SearchForARange.java
@@ -0,0 +1,49 @@
+package leetcode;
+
+public class Q34_SearchForARange {
+	public int search1(int[] nums, int target){
+		int l = 0;
+		int r = nums.length - 1;
+		while (l <=r){
+			int m = (l + r) /2;
+			if (nums[m] == target){
+				if ((m==0)||((m!=0)&&(nums[m-1] < target)))
+					return m;
+				else
+					r = m - 1;
+			}else if (nums[m] < target){
+				l = m + 1;
+			}else if (target < nums[m]){
+				r = m - 1;
+			}
+		}
+		return -1;
+	}
+	public int search2(int[] nums, int target){
+		int l = 0;
+		int r = nums.length - 1;
+		while (l <=r){
+			int m = (l + r) /2;
+			if (nums[m] == target){
+				if ((m==nums.length - 1)||((m!=nums.length - 1)&&(target < nums[m+1])))
+					return m;
+				else
+					l = m + 1;
+			}else if (nums[m] < target){
+				l = m + 1;
+			}else if (target < nums[m]){
+				r = m - 1;
+			}
+		}
+		return -1;
+	}
+	public int[] searchRange(int[] nums, int target) {
+        return new int[] {search1(nums,target),search2(nums,target)};
+    }
+	public static void main(String[] args) {
+		Q34_SearchForARange q = new Q34_SearchForARange();
+		int[] s = q.searchRange(new int[]{5,7,7,8,8,10},8);
+		System.out.println(s[0] + "____" + s[1]);
+	}
+
+}
