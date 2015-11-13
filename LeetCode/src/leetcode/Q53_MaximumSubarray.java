7d0d5d39e0483bfd9a9402eff5c6d9f27f5ace8c
fivemoons <liuyuyao1992@qq.com>
2015-11-13 16:24:53 +0800

    New Q53_MaximumSubarray.java 分治

--- LeetCode/src/leetcode/Q53_MaximumSubarray.java
+++ LeetCode/src/leetcode/Q53_MaximumSubarray.java
@@ -1,7 +1,7 @@
 package leetcode;
 
 public class Q53_MaximumSubarray {
-    public static int maxSubArray(int[] nums) {
+    public static int maxSubArray(int[] nums) { //动态规划
         int[] b = new int[nums.length];
         b[0] = nums[0];
         int max = nums[0];
@@ -11,8 +11,39 @@ public class Q53_MaximumSubarray {
         }
         return max;
     }
+    private static int maxSubArray2(int[] nums, int l, int r){
+    	if (l==r) return nums[l];
+    	
+    	int mid = (l+r) >> 1;
+    	int maxPre = maxSubArray2(nums,l,mid);
+    	int maxEnd = maxSubArray2(nums,mid+1,r);
+    	
+    	int leftMax = Integer.MIN_VALUE;
+    	int rightMax = Integer.MIN_VALUE;
+        int sum = 0;  
+        for (int i = mid; i >= l; i--)  
+        {  
+            sum += nums[i];  
+            if (sum > leftMax)  
+                leftMax = sum;  
+        }  
+        sum = 0;  
+        for (int i = mid+1; i <=r; i++)  
+        {  
+            sum += nums[i];  
+            if (sum > rightMax)  
+                rightMax = sum;  
+        }  
+        sum = leftMax + rightMax;
+    	
+    	return Math.max( Math.max(maxPre, maxEnd) , sum);
+    }
+    public static int maxSubArray2(int[] nums){
+    	if (nums.length<=0) return 0;
+    	return maxSubArray2(nums,0,nums.length-1);
+    }
 	public static void main(String[] args) {
-		System.out.println(maxSubArray(new int[]{1,2}));
+		System.out.println(maxSubArray2(new int[]{1,2}));
 	}
 
 }
