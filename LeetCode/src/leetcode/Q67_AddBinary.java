c542e1c11b371ea2650341fde947593c5681715d
lany <liuyuyao1992@qq.com>
2015-11-17 01:17:29 +0800

    New Q67_AddBinary.java

---
+++ LeetCode/src/leetcode/Q67_AddBinary.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+import java.math.BigInteger;
+
+public class Q67_AddBinary { //BigIntege 很简单。。。
+    public static String addBinary(String a, String b) {
+    		BigInteger biga = new BigInteger(a,2);
+    		BigInteger bigb = new BigInteger(b,2);
+    		BigInteger bigc = biga.add(bigb);
+    		return bigc.toString(2);
+    }
+	public static void main(String[] args) {
+		System.out.println(addBinary("11","1"));
+	}
+
+}
