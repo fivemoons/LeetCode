516126bdf078a804999587e13a4cae1c93635ff0
fivemoons <liuyuyao1992@qq.com>
2015-11-26 19:20:42 +0800

    New Q79_RemoveDuplicatesfromSortedArrayII.java

---
+++ LeetCode/src/leetcode/Q79_RemoveDuplicatesfromSortedArrayII.java
@@ -0,0 +1,22 @@
+package leetcode;
+
+public class Q79_RemoveDuplicatesfromSortedArrayII {
+    public static int removeDuplicates(int[] nums) {
+    	boolean[] bool = new boolean[nums.length]; //是否是要返回的数
+        for(int i=0; i<nums.length; i++){
+        	if ((i<=1) || ((i>1) && (nums[i] != nums[i-2]))) bool[i] = true; //判断条件
+        }
+        int ans = 0; //计数
+        for(int i=0; i<nums.length; i++){
+        	if (bool[i]){
+        		nums[ans] = nums[i]; //移动到前ans个
+        		ans++;
+        	}
+        }
+        return ans;
+    }
+	public static void main(String[] args) {
+		System.out.println(removeDuplicates(new int[]{1,1,1,2,2,3}));
+	}
+
+}
