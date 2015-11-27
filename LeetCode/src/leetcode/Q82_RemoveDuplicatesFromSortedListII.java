b7a6e36ab30e42081211e6acb1c1d1b81c9524e1
fivemoons <liuyuyao1992@qq.com>
2015-11-27 18:34:15 +0800

    1

--- LeetCode/src/leetcode/Q82_RemoveDuplicatesFromSortedListII.java
+++ LeetCode/src/leetcode/Q82_RemoveDuplicatesFromSortedListII.java
@@ -10,23 +10,23 @@ public class Q82_RemoveDuplicatesFromSortedListII {
 	public static ListNode deleteDuplicates(ListNode head) {
 		ListNode t1 = new ListNode(-1);
 		t1.next = head;
-		ListNode t2 = head;
-		while(t2.next != null){
-			System.out.println(t1.val + "___" + t2.val);
-			while((t2.next != null) && (t1.next.val == t2.val)) t2 = t2.next;
+		while(t1.next != null){
+			ListNode t2 = t1.next;
+			while((t2 != null) && (t1.next.val == t2.val)){
+				t2 = t2.next;
+			}
 			t1.next = t2;
-			while((t2.next != null) && (t2.next.val != t2.val)) t1 = t1.next;
 		}
 		return head;
 	}
 	public static void main(String[] args) {
 		ListNode ans = new ListNode(1);
-		ans.next = new ListNode(2);
-		ans.next.next = new ListNode(3);
-		ans.next.next.next = new ListNode(3);
+		ans.next = new ListNode(1);
+		/*ans.next.next = new ListNode(3);
+		#ans.next.next.next = new ListNode(3);
 		ans.next.next.next.next = new ListNode(4);
 		ans.next.next.next.next.next = new ListNode(4);
-		ans.next.next.next.next.next.next = new ListNode(5);
+		ans.next.next.next.next.next.next = new ListNode(5);*/
 		ans = deleteDuplicates(ans);
 		while(ans!=null){
 			System.out.print(ans.val);
