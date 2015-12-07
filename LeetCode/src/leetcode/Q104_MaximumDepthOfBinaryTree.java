94ac6d594e9dddb1db33f0d34446965a66de8cfc
lany <liuyuyao1992@qq.com>
2015-12-07 21:33:04 +0800

    Q104_MaximumDepthOfBinaryTree.java

---
+++ LeetCode/src/leetcode/Q104_MaximumDepthOfBinaryTree.java
@@ -0,0 +1,21 @@
+package leetcode;
+
+public class Q104_MaximumDepthOfBinaryTree {
+	public static class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+		TreeNode(int x) { val = x; }
+	}
+    public int maxDepth(TreeNode root) {
+        if (root == null)
+        		return 0;
+        else
+        		return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+    }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
