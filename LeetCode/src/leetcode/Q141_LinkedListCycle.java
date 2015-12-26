019513252bdb1ba12ec30398161df4145472b6e3
fivemoons <liuyuyao1992@qq.com>
2015-12-26 15:43:01 +0800

    New Q141_LinkedListCycle.java

--- LeetCode/src/leetcode/Q141_LinkedListCycle.java
+++ LeetCode/src/leetcode/Q141_LinkedListCycle.java
@@ -2,20 +2,22 @@ package leetcode;
 
 public class Q141_LinkedListCycle {
 	private static class ListNode {
+		@SuppressWarnings("unused")
 		int val;
-		 ListNode next;
-		 ListNode(int x) {
+		ListNode next;
+		ListNode(int x) {
 			val = x;
-		 	next = null;
-		 }
+			next = null;
+		}
 	}
     public static boolean hasCycle(ListNode head) {
-    		if ((head == null) || (head.next == null)) return false;
-        ListNode t = head.next;
-        while((t!=null) && (t!=head)){
-        		t = t.next;
-        }
-        return t == head;
+    	if ((head == null) || (head.next == null)) return false;
+    	ListNode fast = head, slow = head;
+    	do {
+			slow = slow.next;  //一倍速度走
+			fast = fast.next.next; //两倍速度走
+		} while ( fast!=slow && fast!=null && fast.next != null );
+        return (fast == slow);
     }
 	public static void main(String[] args) {
 		ListNode ans = new ListNode(1);
