1178472fb0b80fb7597f1102ac122eff9eaffd16
lany <liuyuyao1992@qq.com>
2016-02-04 22:08:14 +0800

    New Q292_NimGame.java

---
+++ LeetCode/src/leetcode/Q292_NimGame.java
@@ -0,0 +1,12 @@
+package leetcode;
+
+public class Q292_NimGame {
+	public static boolean canWinNim(int n) {
+		return n % 4 != 0;
+    }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
