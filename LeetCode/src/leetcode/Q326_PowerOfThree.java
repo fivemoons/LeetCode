6019f105c59f3e9bf48ada34ec4d11ed512d15de
lany <liuyuyao1992@qq.com>
2016-02-07 01:47:46 +0800

    New Q326_PowerOfThree.java

---
+++ LeetCode/src/leetcode/Q326_PowerOfThree.java
@@ -0,0 +1,12 @@
+package leetcode;
+
+public class Q326_PowerOfThree {
+	public boolean isPowerOfThree(int n) { //是不是3的指数 
+	    return ( n>0 &&  1162261467%n==0); //3^19
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
