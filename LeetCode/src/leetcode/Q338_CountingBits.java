4a65ea01fbbf737bbaf8197368172af4d0165fde
fivemoons <liuyuyao1992@qq.com>
2016-04-08 09:28:06 +0800

    New Q338_CountingBits.java

--- LeetCode/src/leetcode/Q338_CountingBits.java
+++ LeetCode/src/leetcode/Q338_CountingBits.java
@@ -4,13 +4,8 @@ public class Q338_CountingBits {
 	public static int[] countBits(int num) {
         int[] ans = new int[num+1];
         ans[0] = 0;
-        int i = 1;
-        int n = 1;
-        while(i<num+1){
-            if(i == n * 2)
-                n <<= 1;
-            ans[i] = ans[i-n] + 1;
-            i++;
+        for(int i = 1; i<=num; i++){
+        	ans[i] =  ans[i>>1] + i % 2;
         }
         return ans;
     }
