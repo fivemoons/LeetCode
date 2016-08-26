b4a88fa9e084552f961b16a80a11c672dcfed3ac
lany <liuyuyao1992@qq.com>
2016-08-26 15:37:50 +0800

    New MacBook Pro

--- src/leetcode/Q236_LowestCommonAncestorOfABinaryTree.java
+++ src/leetcode/Q236_LowestCommonAncestorOfABinaryTree.java
@@ -8,10 +8,9 @@ public class Q236_LowestCommonAncestorOfABinaryTree {
 		TreeNode(int x) { val = x; }
 	}
     public static TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if(root==null || p==null || q==null) return null;
-        if(root == p || root == q) return root;
-        TreeNode left = lowestCommonAncestor(root.left,p,q);
-        TreeNode right = lowestCommonAncestor(root.right,p,q);
+        if(root == p || root == q || root == null) return root;
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
         if(left!=null && right!=null) return root;
         return left!=null ? left : right;
     }
--- src/leetcode2/list/Q2_AddTwoNumbers.java
+++ src/leetcode2/list/Q2_AddTwoNumbers.java
@@ -7,34 +7,32 @@ public class Q2_AddTwoNumbers {
 		ListNode(int x) {val = x;}
 	}
 	public ListNode addTwoNumbers(ListNode l1, ListNode l2){
-		ListNode l3 = new ListNode(0);
-		ListNode ans = l3;
-		
-		while(l1!=null && l2!=null){
-			l3.next = new ListNode(l1.val + l2.val);
-			l1 = l1.next;
-			l2 = l2.next;
-			l3 = l3.next;
-		}
-		while(l1!=null){
-			l3.next = new ListNode(l1.val);
-			l1 = l1.next;
-			l3 = l3.next;
-		}
-		while(l2!=null){
-			l3.next = new ListNode(l2.val);
-			l2 = l2.next;
-			l3 = l3.next;
-		}
-		l3 = ans.next;
-		while(l3!=null){
-			if(l3.val>=10){
-				if(l3.next == null) l3.next = new ListNode(0);
-				l3.next.val += l3.val / 10;
-				l3.val = l3.val % 10;
-			}
-			l3 = l3.next;
-		}
-		return ans.next;
+		ListNode p = new ListNode(0);
+        ListNode ans = p;
+        while(l1!=null && l2!=null){
+            p.next = new ListNode(l1.val + l2.val + p.val / 10);
+            p.val = p.val % 10;
+            p = p.next;
+            l1 = l1.next;
+            l2 = l2.next;
+        }
+        while(l1!=null){
+            p.next = new ListNode(l1.val + p.val / 10);
+            p.val = p.val % 10;
+            p = p.next;
+            l1 = l1.next;
+        }
+        while(l2!=null){
+            p.next = new ListNode(l2.val + p.val / 10);
+            p.val = p.val % 10;
+            p = p.next;
+            l2 = l2.next;
+        }
+        if (p.val / 10 > 0){
+            p.next = new ListNode(p.val / 10);
+            p.val = p.val % 10;
+            p = p.next;
+        }
+        return ans.next;
 	}
 }
